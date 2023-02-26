96df41563773698a4e367f1baf72eb2d70832f93
Tymek Lewandowski <tymek@tymeks-air.lan>
2023-02-26 11:33:37 +0000

    Added feather icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./output.css"
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { Triangle } from "react-feather"
 import NotFound from "./pages/NotFound";
 
 function App() {
     return (
         <BrowserRouter>
             <div>
+                <Triangle />
                 <h1 className=" text-9xl">Fimshes</h1>
                 <Routes>
                     <Route path="/" element={<Home />} />
